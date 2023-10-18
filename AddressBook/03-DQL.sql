0239a7adb1ce2187480e3f37e1545caa4e694877
Benjamin DAGUE <bdague@grindboxlab.fr>
2023-10-18 11:47:27 +0200

    address book

---
+++ AddressBook/03-DQL.sql
@@ -0,0 +1,40 @@
+--Data Query Language
+
+--Retourner l'ensemble des villes avec le nom et le code postal
+
+--Retourner l'ensemble des villes avec le nom et le code postal 
+--limité aux 2 premier résultats
+
+--Retourner l'ensemble des noms des villes sans doublon
+
+--Retourner l'ensemble des noms des villes sans doublon ordonnés par nom
+
+--Sélectionner la ville avec l'identifiant 11
+
+--Sélectionner les villes qui n'ont pas l'identifiant 11
+
+--Sélectionner les villes qui ont un identifiant inférieur ou égale à 4
+
+--Sélectionner les contacts qui ont une date de naissance comprise
+--entre 01/01/1990 et 31/12/1999
+
+
+--Sélectionner les villes qui...
+--commencent par LAV
+
+--terminent par VAL
+
+--contiennent AVA
+
+--contiennent au moins 2 A
+
+--terminent par A suivi d'un unique caractère
+
+--commencent par un caractère compris entre a et l
+
+--ne commencent pas par un caractère compris entre a et l
+
+
+
+--Nombre de code postaux différents
+
